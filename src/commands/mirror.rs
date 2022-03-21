4aaf8ad102e075ecd21ba5f76e9a5991d0a4882a
Salivala <hsszyman@gmail.com>
2022-03-21 18:17:05 -0400

    BOT-2 minor cleanups i think

--- src/commands/mirror.rs
+++ src/commands/mirror.rs
@@ -1,16 +1,17 @@
 use core::fmt;
 use std::{str::FromStr, string};
 
+use crate::commands::{get_channel_from_db, parse_channel_as_option};
 use crate::{commands::parse_channel, Connection, MirrorChannelCache};
+
 use db::sea_orm::*;
 use sea_query::*;
 use serenity::{
     framework::standard::{macros::command, Args, CommandResult},
     model::{channel::Message, id::ChannelId},
-    prelude::Context, utils::Colour,
+    prelude::Context,
+    utils::Colour,
 };
-use db::sea_orm::Value::BigInt;
-use crate::commands::{get_channel_from_db, parse_channel_as_option};
 
 use crate::utilities::permission_utilities::*;
 
@@ -18,16 +19,16 @@ enum MirrorArgument {
     Channel(Option<i64>),
     List,
     Help,
-    Remove
+    Remove,
 }
 impl std::str::FromStr for MirrorArgument {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "-r"|"-remove" => Ok(MirrorArgument::Remove),
-            "-l"|"-list" => Ok(MirrorArgument::List),
-            "-h"|"-help"|"-i"|"-info" => Ok(MirrorArgument::Help),
-            x => Ok(MirrorArgument::Channel(parse_channel_as_option(x)))
+            "-r" | "-remove" => Ok(MirrorArgument::Remove),
+            "-l" | "-list" => Ok(MirrorArgument::List),
+            "-h" | "-help" | "-i" | "-info" => Ok(MirrorArgument::Help),
+            x => Ok(MirrorArgument::Channel(parse_channel_as_option(x))),
         }
     }
 }
@@ -40,11 +41,19 @@ impl MirrorArgument {
 #[command]
 #[min_args(1)]
 #[max_args(2)]
-#[aliases(mirror_channel, mirrorChannel, mirrorchannel, mirror_channel, mc, mirror)]
+#[aliases(
+    mirror_channel,
+    mirrorChannel,
+    mirrorchannel,
+    mirror_channel,
+    mc,
+    mirror
+)]
 async fn mirror(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    if !is_message_author_admin(ctx, msg).await{
-        &msg.reply(ctx, "You must be a moderator to run this command.").await?;
-        return Ok(())
+    if !is_message_author_admin(ctx, msg).await {
+        msg.reply(ctx, "You must be a moderator to run this command.")
+            .await?;
+        return Ok(());
     }
     let arg_array = args.raw().collect::<Vec<&str>>();
     let message_channel_id = &i64::from(msg.channel_id);
@@ -52,92 +61,125 @@ async fn mirror(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let first_argument = arg_array[0];
     if arg_array.len() == 2 {
         let second_argument = arg_array[1];
-        match [MirrorArgument::from(first_argument), MirrorArgument::from(second_argument)] {
+        match [
+            MirrorArgument::from(first_argument),
+            MirrorArgument::from(second_argument),
+        ] {
             // source INTO mirror
-            [MirrorArgument::Channel(x), MirrorArgument::Channel(y)] => {
-                match [get_channel_from_db(first_argument, &ctx, message_guild_id).await,
-                       get_channel_from_db(second_argument, &ctx, message_guild_id).await] {
+            [MirrorArgument::Channel(Some(x)), MirrorArgument::Channel(Some(y))] => {
+                match [
+                    get_channel_from_db(first_argument, &ctx, message_guild_id).await,
+                    get_channel_from_db(second_argument, &ctx, message_guild_id).await,
+                ] {
                     [Ok(Some(mut first)), Ok(Some(mut second))] => {
-                        let [source_channel_id, mirror_channel_id] = [x.unwrap(), y.unwrap()];
+                        let [source_channel_id, mirror_channel_id] = [x, y];
                         println!();
                         let mut data = ctx.data.write().await;
                         let con = data.get::<Connection>().unwrap();
                         println!("nice2");
                         first.mirror_to_channel_id = Set(Some((mirror_channel_id)));
                         first.update(con).await?;
-                        data.get_mut::<MirrorChannelCache>().unwrap()
+                        data.get_mut::<MirrorChannelCache>()
+                            .unwrap()
                             .insert(source_channel_id, mirror_channel_id);
-                        println!("wow what's happening");
-                    },
+                    }
                     [Err(e), Err(y)] => {
                         println!("evil error")
-                    },
+                    }
                     [_, _] => {
                         println!("mysterioius")
                     }
                 }
-            },
+            }
             // remove source ( no longer links to a mirror )
             [MirrorArgument::Remove, MirrorArgument::Channel(id)] => {
                 // TODO:
-            },
+            }
             _ => {
-                msg.reply(&ctx, "Invalid mirror arguments supplied. Please refer to mirror --help for usage").await?;
+                msg.reply(
+                    &ctx,
+                    "Invalid mirror arguments supplied. Please refer to mirror --help for usage",
+                )
+                .await?;
             }
         }
-    }
-    else if arg_array.len() == 1{
+    } else if arg_array.len() == 1 {
         match MirrorArgument::from(first_argument) {
             MirrorArgument::Remove => {
                 let mut data = ctx.data.write().await;
                 let con = data.get::<Connection>().unwrap();
-                if let Some(_) = data.get::<MirrorChannelCache>().unwrap().get(message_channel_id) {
-                    let mut x: db::channel::ActiveModel = db::channel::Entity::find_by_id(message_channel_id.to_owned())
-                        .one(con).await
-                        .expect("oh no!").unwrap().into();
+                if let Some(_) = data
+                    .get::<MirrorChannelCache>()
+                    .unwrap()
+                    .get(message_channel_id)
+                {
+                    let mut x: db::channel::ActiveModel =
+                        db::channel::Entity::find_by_id(message_channel_id.to_owned())
+                            .one(con)
+                            .await
+                            .expect("oh no!")
+                            .unwrap()
+                            .into();
                     x.mirror_to_channel_id = Set(None);
                     x.update(con).await?;
-                    data.get_mut::<MirrorChannelCache>().unwrap().remove(message_channel_id);
+                    data.get_mut::<MirrorChannelCache>()
+                        .unwrap()
+                        .remove(message_channel_id);
                 }
-            },
+            }
             MirrorArgument::Channel(id) => {
                 let mut data = ctx.data.write().await;
                 let con = data.get::<Connection>().unwrap();
                 match parse_channel(arg_array[0]) {
                     Ok(mirror_channel) => {
-                        match db::channel::Entity::find_by_id(message_channel_id.to_owned()).one(con).await {
+                        match db::channel::Entity::find_by_id(message_channel_id.to_owned())
+                            .one(con)
+                            .await
+                        {
                             Ok(Some(model)) => {
                                 let mut channel: db::channel::ActiveModel = model.into();
                                 channel.mirror_to_channel_id = Set(Some(mirror_channel));
                                 channel.update(con).await?;
-                                data.get_mut::<MirrorChannelCache>().unwrap().insert(message_channel_id.to_owned(), mirror_channel);
-                            },
+                                data.get_mut::<MirrorChannelCache>()
+                                    .unwrap()
+                                    .insert(message_channel_id.to_owned(), mirror_channel);
+                            }
                             Ok(None) => {
                                 db::channel::ActiveModel {
                                     guild_id: Set(message_guild_id.to_owned()),
                                     channel_id: Set(message_channel_id.to_owned()),
-                                    mirror_to_channel_id: Set(Some(mirror_channel))
-                                }.insert(con).await
+                                    mirror_to_channel_id: Set(Some(mirror_channel)),
+                                }
+                                .insert(con)
+                                .await
                                 .expect("COULDNT DO IT LMAO WTF HAHAHAHHA");
-                                data.get_mut::<MirrorChannelCache>().unwrap().insert(message_channel_id.to_owned(), mirror_channel);
-                            },
+                                data.get_mut::<MirrorChannelCache>()
+                                    .unwrap()
+                                    .insert(message_channel_id.to_owned(), mirror_channel);
+                            }
                             Err(_) => {
                                 msg.reply(&ctx, "Valid Channel Not Supplied");
                             }
                         }
-                    },
+                    }
                     Err(_) => {
-                        msg.reply(ctx, "Please supply a channel to mirror into").await?;
+                        msg.reply(ctx, "Please supply a channel to mirror into")
+                            .await?;
                         return Ok(());
                     }
                 }
-            },
+            }
             MirrorArgument::List => {
                 let data = ctx.data.write().await;
                 let con = data.get::<Connection>().unwrap();
                 let channels = db::channel::Entity::find()
-                    .filter(db::channel::Column::MirrorToChannelId.eq(Some(message_channel_id.to_owned())))
-                    .all(con).await.expect("Mirror channel not found");
+                    .filter(
+                        db::channel::Column::MirrorToChannelId
+                            .eq(Some(message_channel_id.to_owned())),
+                    )
+                    .all(con)
+                    .await
+                    .expect("Mirror channel not found");
 
                 let mut channel_response = string::String::from("");
                 for channel in channels {
@@ -145,14 +187,17 @@ async fn mirror(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
                     let channel_name = &ctx.cache.guild_channel(n).await.unwrap().name;
                     channel_response += &format!("\n- #{}", &channel_name);
                 }
-                ChannelId(*message_channel_id as u64).send_message(&ctx, |m| {
-                    m.embed(|e| {
-                        e.color(Colour::LIGHTER_GREY)
-                        .title("Source Channels")
-                        .description(channel_response)
+                ChannelId(*message_channel_id as u64)
+                    .send_message(&ctx, |m| {
+                        m.embed(|e| {
+                            e.color(Colour::LIGHTER_GREY)
+                                .title("Source Channels")
+                                .description(channel_response)
+                        })
                     })
-                }).await.unwrap();
-            },
+                    .await
+                    .unwrap();
+            }
             MirrorArgument::Help => {
                 ChannelId(*message_channel_id as u64).send_message(&ctx, |m| {
                     m.embed(|e| {
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,61 +1,76 @@
-use std::num::ParseIntError;
+use crate::{Connection, EntityTrait, MirrorChannelCache};
+use db::sea_orm::{ActiveModelTrait, ColumnTrait, IntoSimpleExpr, QueryFilter, Set, Value};
 use serenity::client::Context;
-use crate::{Connection, MirrorChannelCache, EntityTrait};
-use db::sea_orm::{Set, ActiveModelTrait, ColumnTrait, QueryFilter, Value, IntoSimpleExpr};
+use std::num::ParseIntError;
 
 use db::sea_orm::sea_query::error::Error;
 
+pub mod disclaimer;
+pub mod mirror;
+pub mod mod_role;
 pub mod ping;
 pub mod snapshot;
 pub mod snapshot_channel;
-pub mod mod_role;
-pub mod disclaimer;
 pub mod watch;
-pub mod mirror;
 
-pub fn parse_channel(channel_mention: &str) -> Result<i64, ParseIntError>{
+pub fn parse_channel(channel_mention: &str) -> Result<i64, ParseIntError> {
     match channel_mention.parse::<i64>() {
         Ok(t) => Ok(t),
-        Err(_) => channel_mention[2..channel_mention.len()-1].parse::<i64>()
+        Err(_) => channel_mention[2..channel_mention.len() - 1].parse::<i64>(),
     }
 }
 
 pub fn parse_channel_as_option(channel_mention: &str) -> Option<i64> {
-    match channel_mention.parse::<i64>(){
+    match channel_mention.parse::<i64>() {
         Ok(t) => Some(t),
-        Err(_) => {
-            match channel_mention[2..channel_mention.len()-1].parse::<i64>() {
-                Ok(id) => Some(id),
-                Err(_) => None
-            }
-        }
+        Err(_) => match channel_mention[2..channel_mention.len() - 1].parse::<i64>() {
+            Ok(id) => Some(id),
+            Err(_) => None,
+        },
     }
 }
 
 // retrieve OR create a channel from an optional id. if id is none just return None for the model
-async fn get_channel(channel_id: i64, ctx: &Context, guild_id: &i64) -> Result<Option<db::channel::ActiveModel>, Error> {
+async fn get_channel(
+    channel_id: i64,
+    ctx: &Context,
+    guild_id: &i64,
+) -> Result<Option<db::channel::ActiveModel>, Error> {
     let data = ctx.data.write().await;
     let con = data.get::<Connection>().expect("wow");
-    return match db::channel::Entity::find_by_id(channel_id.to_owned()).one(con).await.expect("nice") {
+    return match db::channel::Entity::find_by_id(channel_id.to_owned())
+        .one(con)
+        .await
+        .expect("nice")
+    {
         Some(channel) => {
             let mut active_channel: db::channel::ActiveModel = channel.into();
-            println!("has channel");
-            Ok(Some(active_channel))
-        },
-        None => {
-            println!("wow!!!!!");
-
-            let mut active_channel: db::channel::ActiveModel =
-                db::channel::ActiveModel {
-                    guild_id: Set(guild_id.to_owned()),
-                    channel_id: Set(channel_id.to_owned()),
-                    mirror_to_channel_id: Set(None)
-                }.insert(con).await.expect("Issue creating channel").into();
             Ok(Some(active_channel))
         }
-    }
+        None => {
+            let mut active_channel: db::channel::ActiveModel = db::channel::ActiveModel {
+                guild_id: Set(guild_id.to_owned()),
+                channel_id: Set(channel_id.to_owned()),
+                mirror_to_channel_id: Set(None),
+            }
+            .insert(con)
+            .await
+            .expect("Issue creating channel")
+            .into();
+            Ok(Some(active_channel))
+        }
+    };
 }
 
-pub async fn get_channel_from_db(channel_mention: &str, ctx: &Context, guild_id: &i64) -> Result<Option<db::channel::ActiveModel>, Error> {
-    get_channel(parse_channel(channel_mention).expect("yippy"), ctx, guild_id).await
+pub async fn get_channel_from_db(
+    channel_mention: &str,
+    ctx: &Context,
+    guild_id: &i64,
+) -> Result<Option<db::channel::ActiveModel>, Error> {
+    get_channel(
+        parse_channel(channel_mention).expect("yippy"),
+        ctx,
+        guild_id,
+    )
+    .await
 }
